9403a472d2922da0bb2a01982a9036d5fae4ad36
vctrgd <rigaudvictor13@gmail.com>
2025-12-11 09:13:36 +0100

    ajout des métadonées

--- AWSCTD/workflow/convert_netmob_awsctd.py
+++ AWSCTD/workflow/convert_netmob_awsctd.py
@@ -8,10 +8,11 @@ import sys
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 INPUT_PATH = os.path.abspath(os.path.join(BASE_DIR, '../../netmob23/'))
 OUTPUT_FILE = os.path.join(BASE_DIR, 'netmob_for_awsctd.csv')
+OUTPUT_META_FILE = os.path.join(BASE_DIR, 'netmob_metadata.csv')
 ANOMALY_FILE = os.path.join(BASE_DIR, '../anomalies.txt')
-MAX_FILES = 500
 
 # Paramètres AWSCTD
+MAX_FILES = 500
 VOCAB_SIZE = 100    
 FIXED_LENGTH = 96
 
@@ -46,9 +47,11 @@ def load_data(path):
 
     print(f"Lecture en cours...")
     sequences = []
-    dates_found = []
+    metadata = []
+
 
     for i, file in enumerate(files):
+        filename = os.path.basename(file)
         try:
             with open(file, 'r') as f:
                 lines = f.readlines()
@@ -60,7 +63,7 @@ def load_data(path):
                         traffic_seq = [float(x) for x in parts[1:]]
                         if len(traffic_seq) > 10: 
                             sequences.append(traffic_seq)
-                            dates_found.append(date_str)
+                            metadata.append({'Date': date_str, 'Tile': filename})
                     except ValueError:
                         continue 
         except Exception:
@@ -70,7 +73,7 @@ def load_data(path):
             print(f"   ... {i}/{len(files)} traités", end='\r')
 
     print(f"\n{len(sequences)} séquences extraites.")
-    return sequences, dates_found
+    return sequences, metadata
 
 def process_sequences(sequences, vocab_size, fixed_len):
     print(" Discrétisation (0-99)...")
@@ -100,7 +103,7 @@ def process_sequences(sequences, vocab_size, fixed_len):
     
     return df_discrete.fillna(0).astype(int)
 
-def add_labels_and_save(df, dates_list,ANOMALY_DATES):
+def add_labels_and_save(df,dates_list, metadata,ANOMALY_DATES):
     print(" Ajout des Labels...")
     labels = [ANOMALY_DATES.get(d, 0) for d in dates_list]
     df['Label'] = labels
@@ -111,10 +114,18 @@ def add_labels_and_save(df, dates_list,ANOMALY_DATES):
     print(f"Sauvegarde : {OUTPUT_FILE}")
     # Séparateur virgule standard
     df.to_csv(OUTPUT_FILE, index=False, header=False, sep=',')
+
+    # Sauvegarde des métadonnées
+    df_meta = pd.DataFrame(metadata)
+    df_meta['Label'] = labels # On garde aussi le label pour vérification
+    df_meta.to_csv(OUTPUT_META_FILE, index=True, index_label='Index')
+
     print("Terminé.")
 
 if __name__ == "__main__":
-    seqs, dates = load_data(INPUT_PATH)
     ANOMALY_DATES = load_anomalies(ANOMALY_FILE)
+    seqs, metadata = load_data(INPUT_PATH)
+    
     df_clean = process_sequences(seqs, VOCAB_SIZE, FIXED_LENGTH)
-    add_labels_and_save(df_clean, dates,ANOMALY_DATES)
+    dates_list = [m['Date'] for m in metadata]
+    add_labels_and_save(df_clean,dates_list, metadata,ANOMALY_DATES)
